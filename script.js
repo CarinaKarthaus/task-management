15c0175f3187fe0105cd7d0e8f8ae9215bff13b7
CarinaKarthaus <carina.karthaus@gmail.com>
2020-07-14 19:14:47 +0200

    Delete task button for task-list

--- script.js
+++ script.js
@@ -92,12 +92,12 @@ function showTasks() {
         let category = task.category;
         let details = task.details;
         // Compile HTML-code for the new task-list-item
-        newTaskHTML = compileListItemHTML(category, peopleAssigned, details);
+        newTaskHTML = compileListItemHTML(category, peopleAssigned, details, i);
         // Append HTML-code of a specific task to task list
         list.insertAdjacentHTML("beforeend", newTaskHTML);
     }
 }
-function compileListItemHTML(category, peopleAssigned, details) {
+function compileListItemHTML(category, peopleAssigned, details, i) {
     // Create first part of HTML-code for task-list-item 
     newTaskHTML = compileListItemStart(category);
 
@@ -117,9 +117,23 @@ function compileListItemHTML(category, peopleAssigned, details) {
                     </div> 	
                     <p class="task-category">${category}</p> 	
                     <p class="details">${details}</p> 	
+                    <button type="button" class="close" onclick="deleteTask(${i})">
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                        </svg>
+                    </button>
                 </div>`;
 
     return newTaskHTML;
+}
+// Deletes task from array and reloads page
+function deleteTask(taskIndex) {
+    alert('Task will be deleted');
+    allTasks.splice(taskIndex, 1);
+    let allTasksAsString = JSON.stringify(allTasks);
+    localStorage.setItem('allTasks', allTasksAsString); 
+    location.reload();
 }
 
 // Create first part of HTML-code for task-list-item
