e52f49faecb8531a8b31a2d6cfc76fe943dca9ff
CarinaKarthaus <carina.karthaus@gmail.com>
2020-07-19 13:19:50 +0200

    Bugfix jquery matrix.html (changing arrow-color portrait & desktop view)

--- jquery.js
+++ jquery.js
@@ -9,11 +9,12 @@ $('#deleteConfirmation').on('click', '.btn-ok', function() {
     location.reload();
 });
 
-  /**
-   * Change arrow-color when hovering over arrow-line 
-   */
+/**
+ * For portrait view 
+*  Change arrow-color when hovering over arrow-line 
+*/
   $(document).ready(function(){
-    let taskClassification = ['urgency-high', 'urgency-low', 'importance-high', 'importance-low'];
+    //let taskClassification = ['urgency-high', 'urgency-low', 'importance-high', 'importance-low'];
         // Urgency high
         $('#urgency-high-line').mouseover(function(){
             $('#urgency-high-arrow').css('border-right', '#D8D8D8 1.5em solid');
@@ -23,7 +24,7 @@ $('#deleteConfirmation').on('click', '.btn-ok', function() {
         });
         // Urgency low
         $('#urgency-low-line').mouseover(function(){
-            $('#urgency-low-arrow').css('border-right', '#D8D8D8 1.5em solid');
+            $('#urgency-low-arrow').css('border-right', '#D8D8D8 1.5em solid'); 
         });
         $('#urgency-low-line').mouseout(function(){
             $('#urgency-low-arrow').css("border-right", '#4c5aa7 1.5em solid');
@@ -43,6 +44,42 @@ $('#deleteConfirmation').on('click', '.btn-ok', function() {
             $('#importance-low-arrow').css("border-right", '#4c5aa7 1.5em solid');
         });
   });
+/**
+ * For desktop-view
+ * Change arrow-color when hovering over arrow-line 
+ */
+  $(document).ready(function(){
+    //let taskClassification = ['urgency-high', 'urgency-low', 'importance-high', 'importance-low'];
+        // Urgency high
+        $('#urgency-high-line-desktop').mouseover(function(){
+            $('#urgency-high-arrow-desktop').css('border-right', '#D8D8D8 1.5em solid');
+        });
+        $('#urgency-high-line-desktop').mouseout(function(){
+            $('#urgency-high-arrow-desktop').css("border-right", '#4c5aa7 1.5em solid');
+        });
+        // Urgency low
+        $('#urgency-low-line-desktop').mouseover(function(){
+            $('#urgency-low-arrow-desktop').css('border-right', '#D8D8D8 1.5em solid'); 
+        });
+        $('#urgency-low-line-desktop').mouseout(function(){
+            $('#urgency-low-arrow-desktop').css("border-right", '#4c5aa7 1.5em solid');
+        });
+        // Importance high
+        $('#importance-high-line-desktop').mouseover(function(){
+            $('#importance-high-arrow-desktop').css('border-right', '#D8D8D8 1.5em solid');
+        });
+        $('#importance-high-line-desktop').mouseout(function(){
+            $('#importance-high-arrow-desktop').css("border-right", '#4c5aa7 1.5em solid');
+        })
+        // Importance low
+        ;$('#importance-low-line-desktop').mouseover(function(){
+            $('#importance-low-arrow-desktop').css('border-right', '#D8D8D8 1.5em solid');
+        });
+        $('#importance-low-line-desktop').mouseout(function(){
+            $('#importance-low-arrow-desktop').css("border-right", '#4c5aa7 1.5em solid');
+        });
+  });
+
 
 
   
