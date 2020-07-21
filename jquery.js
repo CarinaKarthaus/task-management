97a9c00da9f07d29ed4ec8459e1d6acad1521dcc
CarinaKarthaus <carina.karthaus@gmail.com>
2020-07-21 15:19:05 +0200

    Optimized design mobile portrait & landscape view

--- jquery.js
+++ jquery.js
@@ -9,77 +9,34 @@ $('#deleteConfirmation').on('click', '.btn-ok', function() {
     location.reload();
 });
 
-/**
- * For portrait view 
-*  Change arrow-color when hovering over arrow-line 
-*/
-  $(document).ready(function(){
-    //let taskClassification = ['urgency-high', 'urgency-low', 'importance-high', 'importance-low'];
-        // Urgency high
-        $('#urgency-high-line').mouseover(function(){
-            $('#urgency-high-arrow').css('border-right', '#D8D8D8 1.5em solid');
-        });
-        $('#urgency-high-line').mouseout(function(){
-            $('#urgency-high-arrow').css("border-right", '#4c5aa7 1.5em solid');
-        });
-        // Urgency low
-        $('#urgency-low-line').mouseover(function(){
-            $('#urgency-low-arrow').css('border-right', '#D8D8D8 1.5em solid'); 
-        });
-        $('#urgency-low-line').mouseout(function(){
-            $('#urgency-low-arrow').css("border-right", '#4c5aa7 1.5em solid');
-        });
-        // Importance high
-        $('#importance-high-line').mouseover(function(){
-            $('#importance-high-arrow').css('border-right', '#D8D8D8 1.5em solid');
-        });
-        $('#importance-high-line').mouseout(function(){
-            $('#importance-high-arrow').css("border-right", '#4c5aa7 1.5em solid');
-        })
-        // Importance low
-        ;$('#importance-low-line').mouseover(function(){
-            $('#importance-low-arrow').css('border-right', '#D8D8D8 1.5em solid');
-        });
-        $('#importance-low-line').mouseout(function(){
-            $('#importance-low-arrow').css("border-right", '#4c5aa7 1.5em solid');
-        });
-  });
-/**
- * For desktop-view
- * Change arrow-color when hovering over arrow-line 
+ /**
+ * Change arrow-color in matrix when hovering over arrow-line 
  */
   $(document).ready(function(){
-    //let taskClassification = ['urgency-high', 'urgency-low', 'importance-high', 'importance-low'];
-        // Urgency high
-        $('#urgency-high-line-desktop').mouseover(function(){
-            $('#urgency-high-arrow-desktop').css('border-right', '#D8D8D8 1.5em solid');
-        });
-        $('#urgency-high-line-desktop').mouseout(function(){
-            $('#urgency-high-arrow-desktop').css("border-right", '#4c5aa7 1.5em solid');
-        });
-        // Urgency low
-        $('#urgency-low-line-desktop').mouseover(function(){
-            $('#urgency-low-arrow-desktop').css('border-right', '#D8D8D8 1.5em solid'); 
-        });
-        $('#urgency-low-line-desktop').mouseout(function(){
-            $('#urgency-low-arrow-desktop').css("border-right", '#4c5aa7 1.5em solid');
-        });
-        // Importance high
-        $('#importance-high-line-desktop').mouseover(function(){
-            $('#importance-high-arrow-desktop').css('border-right', '#D8D8D8 1.5em solid');
-        });
-        $('#importance-high-line-desktop').mouseout(function(){
-            $('#importance-high-arrow-desktop').css("border-right", '#4c5aa7 1.5em solid');
-        })
-        // Importance low
-        ;$('#importance-low-line-desktop').mouseover(function(){
-            $('#importance-low-arrow-desktop').css('border-right', '#D8D8D8 1.5em solid');
-        });
-        $('#importance-low-line-desktop').mouseout(function(){
-            $('#importance-low-arrow-desktop').css("border-right", '#4c5aa7 1.5em solid');
-        });
-  });
-
-
-
+        // for mobile view
+    highlightArrow('#urgency-high-line', '#urgency-high-arrow');
+    highlightArrow('#urgency-low-line', '#urgency-low-arrow');
+    highlightArrow('#importance-high-line', '#importance-high-arrow');
+    highlightArrow('#importance-low-line', '#importance-low-arrow');
+        // For desktop view
+    highlightArrow('#urgency-high-line-desktop', '#urgency-high-arrow-desktop');
+    highlightArrow('#urgency-low-line-desktop', '#urgency-low-arrow-desktop');
+    highlightArrow('#importance-high-line-desktop', '#importance-high-arrow-desktop');
+    highlightArrow('#importance-low-line-desktop', '#importance-low-arrow-desktop');
   
+  }); 
+
+  function highlightArrow(lineId, arrowId) {
+    // Change color of arrow when hovering over arrow-line
+    $(lineId).mouseover(function(){
+        $(arrowId).css('border-right', '#D8D8D8 1.5em solid');
+    });
+    // Set arrow-color back to default when mouse is leaving arrow-line
+    $(lineId).mouseout(function(){
+        $(arrowId).css("border-right", '#4c5aa7 1.5em solid');
+    });
+  }
+
+
+
+
